ac6bdbffded19f2bf5e13f037f1e3ce38ae85857
dfernandez33 <dfernandez33@gatech.edu>
2021-04-12 17:59:44 -0400

    Initial commit, includes camera manager code to get information from intel realsense

---
+++ camera_manager.py
@@ -0,0 +1,44 @@
+import pyrealsense2 as rs
+import numpy as np
+import threading
+import time
+
+class CameraManager:
+    def __init__(self):
+        # Configure depth and color streams
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+
+        # Get device product line for setting a supporting resolution
+        self.pipeline_wrapper = rs.pipeline_wrapper(self.pipeline)
+        self.pipeline_profile = self.config.resolve(self.pipeline_wrapper)
+        self.device = self.pipeline_profile.get_device()
+        self.depth_scale = self.device.first_depth_sensor().get_depth_scale()
+        # this is used to align the depth and color images since they are captured
+        # from slightly different viewports
+        self.aligner = rs.align(rs.stream.color)
+        self.device_product_line = str(self.device.get_info(rs.camera_info.product_line))
+
+        self.config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+        self.config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+
+        # Start streaming
+        self.pipeline.start(self.config)
+
+
+    def get_frames(self, align=False):
+        depth_frame = None
+        color_frame = None
+        while not depth_frame or not color_frame:
+            frames = self.pipeline.wait_for_frames()
+            if align:
+                frames = self.aligner.process(frames)
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+
+            # Convert images to numpy arrays
+            curr_depth_frame = np.asanyarray(depth_frame.get_data())
+            curr_rgb_frame = np.asanyarray(color_frame.get_data())
+        
+        return curr_rgb_frame, curr_depth_frame
---
+++ camera_testing.py
@@ -0,0 +1,64 @@
+## License: Apache 2.0. See LICENSE file in root directory.
+## Copyright(c) 2015-2017 Intel Corporation. All Rights Reserved.
+
+###############################################
+##      Open CV and Numpy integration        ##
+###############################################
+
+import numpy as np
+import cv2
+from camera_manager import CameraManager
+from pyzbar import pyzbar
+import pyrealsense2 as rs
+import time
+import jetson.inference
+import jetson.utils
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+    
+colorizer = rs.colorizer()
+cam_manager = CameraManager()
+model = jetson.inference.detectNet('ssd-mobilenet-v2', threshold=0.4)
+
+time.sleep(2)
+
+while True:
+    start_time = time.time()
+    rgb_frame, depth_frame = cam_manager.get_frames()
+    detections = model.Detect(jetson.utils.cudaFromNumpy(rgb_frame))
+    for detection in detections:
+        text = "{} {}%".format(COCO_INSTANCE_CATEGORY_NAMES[detection.ClassID], detection.Confidence)
+        print(text)
+
+    barcodes = pyzbar.decode(rgb_frame)
+    # loop over the detected barcodes
+    for barcode in barcodes:
+        # extract the bounding box location of the barcode and draw the
+        # bounding box surrounding the barcode on the image
+        (x, y, w, h) = barcode.rect
+        # the barcode data is a bytes object so if we want to draw it on
+        # our output image we need to convert it to a string first
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+        # draw the barcode data and barcode type on the image
+        depth = depth_frame[x:x+w, y:y+h].astype(float)
+        depth = depth * cam_manager.depth_scale
+        dist,_,_,_ = cv2.mean(depth[depth > 0])
+        # print the barcode type and data to the terminal
+        print("[INFO] Found {} barcode: {} at {} meters".format(barcodeType, barcodeData, dist))
+
+    print("Time to process frame: {}s (FPS {})".format(time.time() - start_time, model.GetNetworkFPS()))
+
